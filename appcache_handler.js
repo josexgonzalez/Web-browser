66542300abaf84c9278e97b67c46da7989346032
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2023-11-10 23:56:43 +0100

    Add files via upload

--- appcache_handler.js
+++ appcache_handler.js
@@ -31,40 +31,12 @@ function add_cache_event_toasts() {
     // showToast('Appcache state: ' + get_appcache_state());
     var appCache = window.applicationCache;
     
-    if (!navigator.onLine) {
-        showToast('You are offline.');
-    }
 
-    appCache.addEventListener('cached', function (e) {
-        showToast('Finished caching site.');
-    }, false);
-
-    // appCache.addEventListener('checking', function (e) {
-    //     showToast('Checking for updates.');
-    // }, false);
 
     appCache.addEventListener('downloading', function (e) {
-        showToast('Downloading new cache.');
+        showToast('Welcome to web browser');
     }, false);
 
-    appCache.addEventListener('error', function (e) {
-        // only show error toast if we're online
-        if (navigator.onLine) {
-            showToast('Error while caching site.', 5000);
-        }
-    }, false);
-
-    appCache.addEventListener('noupdate', function (e) {
-        showToast('Cache is up-to-date.');
-    }, false);
-
-    appCache.addEventListener('obsolete', function (e) {
-        showToast('Site is obsolete.');
-    }, false);
-
-    // appCache.addEventListener('progress', function (e) {
-    //     showToast('Caching site.');
-    // }, false);
 
     appCache.addEventListener('updateready', function (e) {
         if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
